fb071c4be36091429d129cd025bc47e175126100
Artem <slivnitsin.art@mail.ru>
2022-08-26 02:51:30 +0800

    task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число, соответствующее дню недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>0 && num<6)
+{
+    Console.WriteLine("Указанный день является рабочим");
+}
+else if(num>5 && num<8)
+{
+    Console.WriteLine("Указанный день является выходным");
+} 
