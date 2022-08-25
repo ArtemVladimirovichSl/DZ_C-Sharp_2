84999454c4d3d84730b0fa1d40c98dbb8b788862
Artem <slivnitsin.art@mail.ru>
2022-08-26 01:58:39 +0800

    task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99 && num < 1000)
+{
+    int num1 = (num/10)%10;
+    Console.Write("Вторая цифра введённого числа - " + num1);
+}
+else
+{
+    Console.Write("Убедитесь, что число трёхзначное.");
+}
