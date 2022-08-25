86f500b7a1683ce941857d90a920f676f5e8fa61
Artem <slivnitsin.art@mail.ru>
2022-08-26 02:24:36 +0800

    task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = (int)Math.Log10(num)-2;
+Console.WriteLine(num1 < 0 ? "В заданном числе третьей цифры нет" : (num % (int)Math.Pow(10, num1 + 1) / (int)Math.Pow(10, num1)).ToString());
